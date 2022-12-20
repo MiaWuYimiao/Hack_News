3fac2b967814ae8713984d65d527421f07d3334c
Yimiao <wuyimiao51s@icloud.com>
2022-12-20 18:23:42 -0500

    add inital files

---
+++ js/nav.js
@@ -0,0 +1,69 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show submit form when click on "submit" */
+
+function navSubmitClick(evt) {
+  console.debug("navSubmitClick", evt);
+  hidePageComponents();
+  $submitForm.show();
+  $allStoriesList.show();
+}
+
+$navSubmit.on("click", navSubmitClick);
+
+/** Show favorite form on click on "favorites" */
+
+function navFavoriteClick(evt) {
+  console.debug("navFavoriteClick", evt);
+  hidePageComponents();
+  $favStoriesList.show();
+}
+
+$navFavorites.on("click", navFavoriteClick);
+
+/** Show submit form on click on "my stories" */
+
+function navMyStoryClick(evt) {
+  console.debug("navMyStoryClick", evt);
+  hidePageComponents();
+  $myStoriesList.show();
+}
+
+$navMyStories.on("click", navMyStoryClick);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
